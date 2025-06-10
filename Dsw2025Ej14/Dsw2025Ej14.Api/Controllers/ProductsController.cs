edcc994c29ce68ba8622028af667450cafd4d01d
Maluma <maluma@google.com>
2025-06-10 15:26:32 -0300

    falta completar

---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Dsw2025Ej14.Api.Data;
+using Dsw2025Ej14.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Ej14.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ProductsController : ControllerBase
+    {
+        PersistenciaEnMemoria persistencia;
+        ProductsController(PersistenciaEnMemoria _persistencia)
+        {
+            persistencia = _persistencia;
+        }
+        [HttpGet("products")]        
+        public IActionResult ObtenerProductos()
+        {
+            var products = persistencia.GetProduct();
+            return Ok(products);
+        }
+        [HttpGet("products/{sku}")]
+        public IActionResult BuscarProducto(string sku)
+        {
+            var product = persistencia.GetProduct(sku);
+            return Ok(product);
+        }
+    }
+}
---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,31 @@
+﻿using Dsw2025Ej14.Api.Domain;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria
+    {
+        private List<Product> ?_products = [];
+    public PersistenciaEnMemoria()
+        {
+            loadProducts();
+        }
+        public void loadProducts(){
+            var json = File.ReadAllText("products.json");
+            _products = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            })?? [];
+        }
+
+        public Product GetProduct(string sku)
+        {
+            return _products?.Find(p => p.Sku == sku);
+        }
+        public IEnumerable<Product> GetProduct()
+        {
+            return _products?.Where(p => p.IsActive);
+        }
+    }
+}
